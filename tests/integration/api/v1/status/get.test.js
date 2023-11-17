0752d6b678828cebfe3c306f0cac371d58af9589
tex008 <texera009@gmail.com>
2023-11-17 09:54:04 -0300

    add `/api/v1/status` endpoint

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,6 @@
+describe("test the status route", () => {
+  it("Should return 200 when send a request GET to /api/v1/status", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+  });
+});
