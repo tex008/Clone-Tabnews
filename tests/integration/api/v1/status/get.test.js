e69e56d027ce6b3ded749bd103cd60ce332a8968
tex008 <texera009@gmail.com>
2024-06-18 00:54:44 -0300

    add `database.js` and `.env` files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,5 @@
+// checar a porta que o backend abriu
+
 describe("test the status route", () => {
   it("Should return 200 when send a request GET to /api/v1/status", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
