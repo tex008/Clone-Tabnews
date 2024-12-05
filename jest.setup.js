02bfdb272db7721736c72af30c3dd22a5e422f6f
tex008 <texera009@gmail.com>
2024-12-05 17:26:37 -0300

    refactor: use `orchestrator.clearDatabase()` in tests

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-import orchestrator from "tests/orchestrator";
-
-beforeAll(async () => {
-  await orchestrator.waitForAllServices();
-});
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,9 @@
-import database from "infra/database.js";
+import orchestrator from "../../../../orchestrator";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public; ");
-}
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+});
 
 describe("GET /api/v1/migrations", () => {
   describe("Annonymous user", () => {
