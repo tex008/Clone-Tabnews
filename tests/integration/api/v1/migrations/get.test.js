46ff97f9751cc2fdd1110f892c0e8b616d758d7f
tex008 <texera009@gmail.com>
2024-12-05 17:12:05 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,13 +6,17 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public; ");
 }
 
-describe("test the migartions route", () => {
-  it("Should return 200 when send a request GET to /api/v1/migrations", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/migrations");
-    expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Annonymous user", () => {
+    describe("Retrieving pending migrations", () => {
+      it("Should return 200 when send a request GET to /api/v1/migrations", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/migrations");
+        expect(response.status).toBe(200);
 
-    const responseBody = await response.json();
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+    });
   });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,15 +1,21 @@
-describe("test the status route", () => {
-  it("Should return 200 when send a request GET to /api/v1/status", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Annonymous user", () => {
+    describe("Retrieving current system status", () => {
+      it("Should return 200 when send a request GET to /api/v1/status", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/status");
+        expect(response.status).toBe(200);
 
-    const responseBody = await response.json();
+        const responseBody = await response.json();
 
-    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-    expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
-    expect(["16.1", "16.4"]).toContain(
-      responseBody.dependencies.database.version,
-    );
-    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+        const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+        expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+        expect(["16.1", "16.4"]).toContain(
+          responseBody.dependencies.database.version,
+        );
+        expect(responseBody.dependencies.database.opened_connections).toEqual(
+          1,
+        );
+      });
+    });
   });
 });
