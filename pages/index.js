f78a02684d64a57265e1f7c4ab8487b85f313acf
tex008 <texera009@gmail.com>
2026-09-09 23:09:16 -0300

    feat: add new status frontend preview

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
+import useSWR from "swr";
+
+async function fetchApi(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
 import React from "react";
 
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchApi, {
+    refreshInterval: 100,
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    console.log(data);
+    updatedAtText = {
+      updatedAtDate: new Date(data.updated_at).toLocaleString("pt-BR"),
+      postgresConnectionsNumber: data.dependencies.database.opened_connections,
+      postgresVersion: data.dependencies.database.version,
+    };
+  }
+  return (
+    <pre>
+      Última atualização:
+      <ul>
+        <li>
+          Data da última atualização:{" "}
+          {JSON.stringify(updatedAtText.updatedAtDate)}
+        </li>
+        <li>
+          Número de conexões Postgres:{" "}
+          {JSON.stringify(updatedAtText.postgresConnectionsNumber)}
+        </li>
+        <li>
+          Versão Postgres: {JSON.stringify(updatedAtText.postgresVersion)}
+        </li>
+      </ul>
+    </pre>
+  );
+}
+
 export default function Home() {
-  return <h1>Minha pandinha, eu amo muito você. ♥</h1>;
+  return (
+    <>
+      <h1>Status: </h1>
+      <UpdatedAt />
+    </>
+  );
 }
